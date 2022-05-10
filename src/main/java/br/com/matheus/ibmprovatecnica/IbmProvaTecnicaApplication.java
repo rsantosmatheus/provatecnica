924fac696074cbdf7102db844f3e43aa3b0aa698
Matheus <rsantosmatheus@outlook.com>
2022-05-10 16:06:25 -0300

    initial commit

---
+++ src/main/java/br/com/matheus/ibmprovatecnica/IbmProvaTecnicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheus.ibmprovatecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmProvaTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbmProvaTecnicaApplication.class, args);
+	}
+
+}
